09264128ac1807d3ad25dbfacd866d2f950aff42
aswath0001 <rsaswaths38@gmail.com>
2026-03-03 22:57:20 +0530

    programs

--- Interview/Q13.java
+++ Interview/Q13.java
@@ -11,6 +11,34 @@ class Q13 {
        for (int i = 0; i <arr.length; i++) {
              arr[i] = scan.nextInt();
             }
-      
+
+     int visited = Integer.MIN_VALUE;
+
+     for (int i = 0; i < arr.length; i++) {
+         if(arr[i] != visited ){
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i]==arr[j]){
+                    arr[i] = visited;
+                }
+            }
+         }
+     }
+       System.out.print("after removing the duplicate numbers [");
+       for (int i = 0; i <arr.length; i++) {
+           if(arr[i] != visited){
+              System.out.print(arr[i]);
+              if(i<arr.length-1){
+                for (int j = i+1; j < arr.length; j++) {
+                     if(arr[j] != visited){
+              System.out.print(", ");
+              break;
+                     }
+                }
+              }
+           }
+       }
+        System.out.println("]");
+        
+        scan.close();
     }
 }
