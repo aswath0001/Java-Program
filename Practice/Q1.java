fa657abb172814cb03a253d26e792cd5c8129a25
aswath0001 <rsaswaths38@gmail.com>
2026-03-09 22:45:22 +0530

    programs

---
+++ Practice/Q1.java
@@ -0,0 +1,17 @@
+class Q1 {
+    public static void main(String[] args) {
+        int a=1;
+        int b=2;
+        int c=0;
+
+        c=a;
+        a=b;
+        b=c;
+
+        System.out.println(a);
+         System.out.println(b);
+    }
+
+   
+
+}
---
+++ Practice/Q3.java
@@ -0,0 +1,12 @@
+class Q3 {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+
+        if(a>b){
+            System.out.println("A is greatest");
+        }else{
+            System.out.println("B is the greatest");
+        }
+    }
+}
