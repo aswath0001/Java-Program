eabe4c9a8779928cb8ec775f3c8267110c827316
aswath0001 <rsaswaths38@gmail.com>
2026-03-12 23:46:10 +0530

    programs

---
+++ Practice/Q19.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+class Q19 {
+    public static void main(String[] args) {
+        Scanner scan=  new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        int a = scan.nextInt();
+         System.out.print("Enter the second number:");
+        int b = scan.nextInt();
+        int result=a>b ? a :b;
+        System.out.println("The greatest number is "+result);
+    }
+}
