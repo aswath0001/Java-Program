c4016edfb0037f8c52e7d509d7ef4a979318a31a
aswath0001 <rsaswaths38@gmail.com>
2026-04-28 11:53:11 +0530

    Infosys

---
+++ Infosys/Infoys 2nd round/Q6.java
@@ -0,0 +1,22 @@
+class Q6 {//brute force
+    public static void hasPair (int [] arr,int target){
+        boolean res = true;
+         for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j <arr.length; j++) {
+                if(arr[i]+arr[j]==target){
+                    System.out.println("Pair found:"+arr[i]+", "+arr[j]);
+                    res=true;
+                }
+            }
+             if (!res) {
+        System.out.println("No pair found");
+    }
+        }
+   
+    }
+    public static void main(String[] args) {
+        int [] arr ={1,2,4,4,7,8,11,12};
+        int target=8;
+       hasPair(arr, target);
+    }
+}
